8c3cca47381e2cfef282feb185be207088eed7d5
jayKimGit <rand31@naver.com>
2023-02-19 15:58:02 +0900

    first commit

---
+++ src/components/Square.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Square.css";
+
+//export default class Square extends React.Component {
+const Square = ({onClick, value}) => {
+    return (
+        <button className="square"
+            onClick={onClick}>
+            {value}
+        </button>
+    )
+}
+
+export default Square;
